954e5ba41b7e30b93b1bce78b9427c43513eefa4
Сергей Иванов <isv1601@yandex.ru>
2023-05-08 10:10:49 +0300

    Перенес константу POST_ON_PAGE в settings.py

--- blogicum/core/utils.py
+++ blogicum/core/utils.py
@@ -1,10 +1,9 @@
 from django.core.paginator import Paginator
-
-POST_ON_PAGE = 10
+from django.conf import settings
 
 
 def paginator(request, object_list):
-    paginator_obj = Paginator(object_list, POST_ON_PAGE)
+    paginator_obj = Paginator(object_list, settings.POST_ON_PAGE)
     page_number = request.GET.get('page')
     page_obj = paginator_obj.get_page(page_number)
     return page_obj
